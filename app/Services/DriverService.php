a24802e9de4b364b34dc1203af2edfab9828a839
jadis jale <jadisjale@gmail.com>
2023-05-20 00:27:17 -0300

    feat: Classe responsavel por validar e criar registro

---
+++ app/Services/DriverService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Driver;
+use App\Models\Vehicle;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class DriverService extends Service
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function save(Request $request)
+    {
+        try {
+            $validation = 'required|min:3';
+            $this->validate($request, [
+                'name' => $validation,
+                'document' => $validation,
+                'plate' => $validation,
+                'model' => $validation,
+            ]);
+            DB::beginTransaction();
+
+            $inputVehicle = $request->only('plate', 'model');
+            $vehicle = Vehicle::create($inputVehicle);
+
+            $inputDriver = $request->only('name', 'document');
+            $inputDriver['vehicle_id'] = $vehicle->id;
+            $driver = Driver::create($inputDriver);
+
+            DB::commit();
+            return $this->responseCreat($driver->toArray());
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return $this->responseErro();
+        }
+    }
+}
