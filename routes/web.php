4a265f1becc8c0712159534935eda4acf3b04047
jadis jale <jadisjale@gmail.com>
2023-05-20 00:24:43 -0300

    feat: rota para salvar motorista e veiculo

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+use App\Http\Controllers\DriverController;
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -16,3 +18,8 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api/v1/'], function () use ($router) {
+    $router->post('/drive', 'DriverController@store');
+});
+
