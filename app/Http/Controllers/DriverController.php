df987c6991853d95eb030c1ef3575401977e5cb8
jadis jale <jadisjale@gmail.com>
2023-05-20 00:30:00 -0300

    feat: controller para driver

---
+++ app/Http/Controllers/DriverController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\DriverService;
+use Illuminate\Http\Request;
+
+class DriverController extends Controller
+{
+
+    private $service;
+
+    public function __construct(DriverService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function store(Request $request)
+    {
+        return $this->service->save($request);
+    }
+}
