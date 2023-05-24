033d830866410898156bd5df961d1d1aa6eb4ef1
Jadis Jale <jadisjale@hotmail.com>
2023-05-24 02:42:25 -0300

    test: adicionado teste para as funcoes testAll e testSearchNameOrDocumentOrPlate

--- tests/services/DriverServiceTest.php
+++ tests/services/DriverServiceTest.php
@@ -1,7 +1,10 @@
 <?php
 
+namespace tests\services\ResponseDriverDTO;
+
 use App\Services\DriverService;
 use Illuminate\Http\Request;
+use TestCase;
 
 class DriverServiceTest extends TestCase
 {
@@ -22,6 +25,25 @@ class DriverServiceTest extends TestCase
         $this->assertEquals($response, $result);
     }
 
+    public function testAll()
+    {
+        $all = $this->getResponseAllDriver();
+        $this->fakeService->method('all')->willReturn($all);
+        $result = $this->fakeService->all();
+        $this->assertEquals($all, $result);
+        $this->assertEquals(count($all['data']), count($result['data']));
+    }
+
+    public function testSearchNameOrDocumentOrPlate() 
+    {
+        $param = 'teste';
+        $filter = $this->getResponseAllDriver();
+        $this->fakeService->method('searchNameOrDocumentOrPlate')->willReturn($filter);
+        $result = $this->fakeService->searchNameOrDocumentOrPlate($param);
+        $this->assertEquals($filter, $result);
+        $this->assertTrue($result['status'] === 200);
+    }
+
     public function getResponsePostDriver(): array
     {
         $response = '{
@@ -34,6 +56,31 @@ class DriverServiceTest extends TestCase
                 "id": 5
             },
             "status": 201
+        }';
+        return json_decode($response, true);
+    }
+
+    public function getResponseAllDriver() 
+    {
+        $response = '{
+            "data": [
+                {
+                    "id": 3,
+                    "name": "Lilian",
+                    "document": "123123",
+                    "vehicle_id": 1,
+                    "created_at": null,
+                    "updated_at": null,
+                    "vehicle": {
+                        "id": 1,
+                        "plate": "123",
+                        "model": "fiat - uno",
+                        "created_at": null,
+                        "updated_at": null
+                    }
+                }
+            ],
+            "status": 200
         }';
         return json_decode($response, true);
     }
